17fc68a071d71da076f52df2b937a12dccf30195
Fajar Falahiat <fajar.falahiat@trycata.com>
2025-12-26 14:12:12 +0700

    chore: setup file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+    ],
+  },
+  experimental: {
+    viewTransition: true,
+  },
 };
 
 export default nextConfig;
