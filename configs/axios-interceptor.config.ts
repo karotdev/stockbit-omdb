0d817904a5d4b27b5b99572d5bfd3759b4cb471b
Fajar Falahiat <fajar.falahiat@trycata.com>
2025-12-27 10:49:50 +0700

    fix: change http to https

--- configs/axios-interceptor.config.ts
+++ configs/axios-interceptor.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_KEY } from "@/constants";
 
 const axiosInstance = axios.create({
-  baseURL: "http://www.omdbapi.com",
+  baseURL: "https://www.omdbapi.com",
   params: { apikey: API_KEY },
 });
 
