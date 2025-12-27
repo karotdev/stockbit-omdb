720c8bb02bdcd63eecb7e557fa07c9e4aae66d46
Fajar Falahiat <fajar.falahiat@trycata.com>
2025-12-27 11:21:26 +0700

    chore: update free api key

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,4 @@
-export const API_KEY = "faf7e5bb";
+// export const API_KEY = "faf7e5bb";
+export const API_KEY = "3e4f484d";
 export const DEFAULT_KEYWORD = "batman";
 export const MIN_KEYWORD_LENGTH = 2;
