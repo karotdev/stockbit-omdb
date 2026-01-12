a9c89a0165be624c05886bd78d59cb4c4acc5103
Fajar Falahiat <fajar.falahiat@trycata.com>
2026-01-12 08:20:29 +0700

    update api key

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,5 @@
 // export const API_KEY = "faf7e5bb";
-export const API_KEY = "3e4f484d";
+// export const API_KEY = "3e4f484d";
+export const API_KEY = "b3e0ac7a";
 export const DEFAULT_KEYWORD = "batman";
 export const MIN_KEYWORD_LENGTH = 2;
